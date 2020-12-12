d964cf860fbde57afee26c5b40527d43e4d916f4
Dan Tcheche <dtchechenistky@devartis.com>
2020-12-12 20:49:47 -0300

    Create basic models

--- my_project/apps/drf/models.py
+++ my_project/apps/drf/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Country(models.Model):
+    name = models.CharField(max_length=50)
+    code = models.CharField(max_length=3, blank=True, null=True, unique=True)
+
+
+class Account(models.Model):
+    name = models.CharField(max_length=255)
+    zip_code = models.CharField(max_length=30, blank=True, null=True)
+    address = models.CharField(max_length=255, blank=True, null=True)
+    country = models.ForeignKey(Country, related_name='accounts', on_delete=models.PROTECT, blank=True, null=True)
+
+
+class User(models.Model):
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    account = models.ForeignKey(Account, related_name='users', on_delete=models.CASCADE, blank=True, null=True)
