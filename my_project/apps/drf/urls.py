3cc9186fb247f2646132f27459b190ba7f8eb027
Dan Tcheche <dtcheche@gmail.com>
2021-02-02 17:43:31 -0300

    Add AccountViewSet

--- my_project/apps/drf/urls.py
+++ my_project/apps/drf/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path, include
+from rest_framework import routers
 
-from my_project.apps.drf.views import CountryView, AccountView
+from my_project.apps.drf.views import CountryView, AccountViewSet
+
+router = routers.SimpleRouter()
+router.register(r'accounts', AccountViewSet)
 
 urlpatterns = [
+    path('', include(router.urls)),
     path('countries/', CountryView.as_view(), name="countries_api"),
-    path('accounts/', AccountView.as_view(), name="accounts_api"),
 ]
--- my_project/apps/drf/views.py
+++ my_project/apps/drf/views.py
@@ -1,4 +1,6 @@
 from rest_framework import generics
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.response import Response
 
 from my_project.apps.drf.models import Country, Account
 from my_project.apps.drf.serializers import CountrySerializer, AccountSerializer
@@ -9,6 +11,23 @@ class CountryView(generics.ListCreateAPIView):
     serializer_class = CountrySerializer
 
 
-class AccountView(generics.ListCreateAPIView):
+class AccountViewSet(ModelViewSet):
     queryset = Account.objects.all()
     serializer_class = AccountSerializer
+
+    def create(self, request, *args, **kwargs):
+        account_name = self.request.data.get('name')
+        zip_code = self.request.data.get('zip_code')
+        address = self.request.data.get('address')
+        country_id = self.request.data.get('country_id')
+        country = Country.objects.filter(id=country_id).first()
+        if not country:
+            return Response("Country not found", status=404)
+        if account_name:
+            account = Account.objects.create(name=account_name,
+                                             country=country,
+                                             zip_code=zip_code,
+                                             address=address)
+            serializer = self.get_serializer(instance=account)
+            return Response(serializer.data, status=201)
+        return Response("Missing account name", status=400)
